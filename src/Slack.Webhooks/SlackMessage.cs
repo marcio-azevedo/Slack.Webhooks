644f9bb8eed298d15b0136def332d4217937551b
Benn <benn@redrabbits.co.uk>
2015-05-16 14:25:58 +0100

    first potential solution for #8

--- src/Slack.Webhooks/SlackMessage.cs
+++ src/Slack.Webhooks/SlackMessage.cs
@@ -1,17 +1,32 @@
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Slack.Webhooks
 {
+    public enum TextType
+    {
+        Normal = 0,
+        Escaped = 1
+    }
     /// <summary>
     /// Slack Message
     /// </summary>
     public class SlackMessage
     {
+        
+        private string _text;
+        
         private bool _markdown = true;
         /// <summary>
         /// This is the text that will be posted to the channel
         /// </summary>
-        public string Text { get; set; }
+        public string Text 
+        { 
+            get{ return _text; } 
+            set{ _text = TextType == TextType.Escaped ? Regex.Unescape(value) : value; } 
+        }
+        
+        public TextType TextType { get; set; }
         /// <summary>
         /// Optional override of destination channel
         /// </summary>
@@ -36,5 +51,14 @@ namespace Slack.Webhooks
         /// Optional attachment collection
         /// </summary>
         public List<SlackAttachment> Attachments { get; set; }
+        
+        public SlackMessage() : this(TextType.Normal)
+        {
+        }
+        
+        public SlackMessage(TextType textType)
+        {
+            TextType = textType;
+        }
     }
 }
