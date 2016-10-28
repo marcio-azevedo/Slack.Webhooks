580d2a50ba891b2f7704e5949f9ab6186b150cce
Benn Lazell <benn.lazell@imimobile.com>
2016-10-28 15:22:55 +0100

    Extract SlackClient Interface

---
+++ src/Slack.Webhooks/ISlackClient.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace Slack.Webhooks
+{
+    public interface ISlackClient
+    {
+        bool Post(SlackMessage slackMessage);
+        Task<IRestResponse> PostAsync(SlackMessage slackMessage);
+        bool PostToChannels(SlackMessage message, IEnumerable<string> channels);
+        IEnumerable<Task<IRestResponse>> PostToChannelsAsync(SlackMessage message, IEnumerable<string> channels);
+    }
+}
